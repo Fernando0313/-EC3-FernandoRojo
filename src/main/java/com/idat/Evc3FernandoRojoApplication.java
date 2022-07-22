d451245558a683e4106122ebcf2cf9ee1a79f3d3
ferna <ferna@LAPTOP-KOPV4T9J>
2022-07-21 19:55:14 -0500

    init

---
+++ src/main/java/com/idat/Evc3FernandoRojoApplication.java
@@ -0,0 +1,13 @@
+package com.idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evc3FernandoRojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evc3FernandoRojoApplication.class, args);
+	}
+
+}
